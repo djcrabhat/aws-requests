7c0453d89881de8a3bb7aca8a3851650e0c70f3c
David Villapudua <dvillapudua@cleardata.com>
2015-12-08 18:26:04 -0700

    Updates to be able to download and pip install this package.

--- setup.py
+++ setup.py
@@ -14,9 +14,12 @@ setup(
     version='0.0.2',
     packages=['awsrequests'],
     url='https://github.com/djcrabhat/aws-requests',
-    license='djcrabhat@sosimplerecords.com',
-    author='djcrabhat',
-    author_email='',
-    description='a requests-wrapper for AWS signed HTTP calls',
-    install_requires=install_requires
+    license='MIT License',
+    author='ClearDATA',
+    author_email='support@cleardata.com',
+    description='For making signed requests to AWS API Gateway endpoints',
+    install_requires=install_requires,
+    download_url='https://github.com/djcrabhat/awsrequests/tarball/0.0.2',
+    keywords=['AWS API Gateway signing', 'API Gateway signing', 'AWS API Gateway requests', 'API Gateway requests'],
+    classifiers=[],
 )
