83ee7171fb623d929d4bb742e054deb5e16e2416
Ross Vandegrift <ross.vandegrift@cleardata.com>
2020-02-14 11:05:15 -0800

    Cleanup setup.py, bump version to 0.0.13 for release

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools import setup, find_packages
-import sys
+from setuptools import setup
 
 install_requires = [
     'requests',
@@ -11,7 +10,7 @@ with open('README.rst') as f:
 
 setup(
     name='awsrequests',
-    version='0.0.11',
+    version='0.0.13',
     packages=['awsrequests'],
     url='https://github.com/djcrabhat/aws-requests',
     license='MIT License',
@@ -19,7 +18,6 @@ setup(
     author_email='support@cleardata.com',
     description='For making signed requests to AWS API Gateway endpoints',
     install_requires=install_requires,
-    #download_url='https://github.com/djcrabhat/awsrequests/tarball/0.0.4',
     keywords=['AWS API Gateway signing', 'API Gateway signing', 'AWS API Gateway requests', 'API Gateway requests'],
     classifiers=[],
 )
